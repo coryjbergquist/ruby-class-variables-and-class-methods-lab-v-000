5088dc894100e22fa50db7d5472d12337ae1482e
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-23 21:18:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 class Song
 
-
+attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
   @@artists = []
 
 
-  def inisitialize(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
