b0ec67677e114d1e513aa227f0508a2d13e585ca
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-23 21:15:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,25 @@ attr_accessor :name, :artist, :genre
     @@artists << artist
   end
 
+  def name=(name)
+    @name = name
+  end
+  def name 
+    @name
+  end
+  def artist=(artist)
+    @artist = artist
+  end
+  def artist
+    @artist
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+
   def count
     @@count
   end
